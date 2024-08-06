daea0f90f5d89feeaa00cfc28b26b84dbd16b75b
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2024-08-06 10:22:35 +0200

    Initial commit

---
+++ src/test/java/dev/serrodcal/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package dev.serrodcal;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
